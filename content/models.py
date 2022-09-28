d59acd070c98707175a64f59fbb70367e6f3055b
Luccas Queiroz <luccasqueiroz7@hotmail.com>
2022-09-28 17:12:18 -0300

    feat: adicionando estruturação do projeto e primeiro modal, com sua respectiva migration

---
+++ content/models.py
@@ -0,0 +1,13 @@
+from email.policy import default
+from django.db import models
+
+
+class Content(models.Model):
+    title = models.CharField(max_length=50)
+    module = models.TextField()
+    students = models.IntegerField()
+    description = models.TextField(null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __repr__(self):
+        return f"<[{self.id}] {self.title} - {self.module}>"
